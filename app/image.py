67b688fcb40ce97080bf12c96c80155caa8f0c18
fangzai hong <fangzai.hong2011@gmail.com>
2017-03-14 21:44:15 -0400

    added entry point for load testing

--- app/image.py
+++ app/image.py
@@ -44,7 +44,7 @@ def images_create_save():
 	else:
 		return render_template("login.html")
 
-	#s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
+	
 	s3 = boto3.resource('s3')
 
 	if 'file' not in request.files:
@@ -220,10 +220,126 @@ def transformed():
 	
 
 
+@webapp.route('/test/FileUpload',methods=['POST'])
+def file_upload_test():
+	
+	userName = request.form.get('userID',"")
+	password = request.form.get('password',"")
 
+	if userName == "" or password == "":
+		error_msg="Error: All fields are required!"
+		return redirect(url_for('file_upload_test'))
 
+	cnx = get_db()
+	cursor = cnx.cursor()
 
+	query = "SELECT * FROM users WHERE username = %s"
 
+	cursor.execute(query,(userName,))
 
+	row = cursor.fetchone()
 
+	if row is None:
+		error_msg="User not exist!"
+		return redirect(url_for('file_upload_test'))
+
+	userId = row[0]
+	hashed = row[3]
+	
+	if not (hashed.encode('utf8') == bcrypt.hashpw(password.encode('utf8'),hashed.encode('utf8'))):
+		
+		error_msg="Incorrect Password!"
+		return redirect(url_for('file_upload_test'))
+
+	session['username'] = userName
+
+	s3 = boto3.resource('s3')
+
+	if 'uploadedfile' not in request.files:
+		msg = "no file"
+		return redirect(url_for('file_upload_test'))
+
+	file = request.files['file']
     
+	if file.filename == '':
+		msg = "empty"
+		return redirect(url_for('file_upload_test'))
+
+	if file and allowed_file(file.filename):
+
+		filename = secure_filename(file.filename)
+		path = db_config['path'] + '/'
+
+		query = "SELECT * FROM images WHERE key1 = %s"
+		cursor.execute(query,(filename,))
+		row = cursor.fetchone()
+		if not row is None:
+			msg = "file exists! try another one"
+			return redirect(url_for('file_upload_test'))
+
+		
+		file.save(os.path.join(path, filename))
+
+		fname = file.filename.rsplit('.', 1)[0]
+		ext = file.filename.rsplit('.', 1)[1]
+		path_name = path + filename
+
+		key1 = username  + "_" + fname + "_origin." + ext
+		key2 = username  + "_" + fname + "_flopped." + ext
+		key3 = username  + "_" + fname + "_resized." + ext
+		key4 = username  + "_" + fname + "_color." + ext
+
+		data = open(path_name, 'rb')
+		#f=open(path_name,'rb')
+		#s3.upload_fileobj(f, 'lizw-a1', key1)
+		s3.Bucket('lizw-a1').put_object(Key=key1, Body=data)
+
+		with Image(filename = path_name) as image:
+			with image.clone() as flopped:
+				flopped.flop()
+				new_file = path + username  + "_" + fname + "_flopped." + ext
+				flopped.save(filename = new_file)
+				data = open(new_file, 'rb')
+				s3.Bucket('lizw-a1').put_object(Key=key2, Body=data)
+				os.remove(new_file)
+
+			with image.clone() as resize:
+				resize.resize(180, 180)
+				new_file = path + username  + "_" + fname + "_resized." + ext
+				resize.save(filename = new_file)
+				data = open(new_file, 'rb')
+				s3.Bucket('lizw-a1').put_object(Key=key3, Body=data)
+				os.remove(new_file)
+
+			with image.clone() as color:
+				frequency = 3
+				phase_shift = -90
+				amplitude = 0.2
+				bias = 0.7
+				color.function('sinusoid', [frequency, phase_shift, amplitude, bias])
+				new_file = path + username  + "_" + fname + "_color." + ext
+				color.save(filename = new_file)
+				data = open(new_file, 'rb')
+				s3.Bucket('lizw-a1').put_object(Key=key4, Body=data)
+				os.remove(new_file)
+		
+		os.remove(path_name)
+
+
+		query = ''' INSERT INTO images (userId, key1, key2, key3, key4)
+		               VALUES (%s, %s, %s, %s, %s)
+		'''
+
+		cursor.execute(query,(userId,key1,key2,key3,key4))
+		cnx.commit()
+
+		msg = "success"
+
+	
+	return redirect(url_for('file_upload_test'))
+
+
+@webapp.route('/test',methods=['GET'])
+def welcome_page():
+	return render_template("test.html")
+
